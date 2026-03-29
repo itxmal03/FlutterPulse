2480d0cc3f9fff23ab04c14995076cf4ccb23e1b
Muhammad Aftab Liaqat <itxmal03@users.noreply.github.com>
2026-03-29 21:23:37 +0500

    file service for picking directory path done

---
+++ lib/service/file_service.dart
@@ -0,0 +1,17 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter_pulse/core/result.dart';
+
+class FileService {
+  static Future<Result<String>> getDirectoryPath() async {
+    try {
+      final path = await FilePicker.platform.getDirectoryPath();
+
+      if (path == null) {
+        return Result.failure("User Cancelled");
+      }
+      return Result.success(path);
+    } catch (e) {
+      return Result.failure("Failed to pick directory");
+    }
+  }
+}
